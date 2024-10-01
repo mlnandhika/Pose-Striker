8d0da3495451e0c0808bff376e1dfee2f424b59b
evanrgerard <159915755+evanrgerard@users.noreply.github.com>
2024-10-01 09:30:45 +0700

    Update main_menu.py

--- gui/main_menu.py
+++ gui/main_menu.py
@@ -8,109 +8,59 @@ class MainMenu(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        # Background Color
-        self.config(bg="#111")
+        # Load and resize background image using Pillow
+        image = Image.open(r"assets/decor/main_menu.png")
+        resized_image = image.resize((1600,900), Image.LANCZOS)  # Resize image using LANCZOS
 
-        # Title
-        self.title = tk.Label(self,fg="snow", text="    Pose Striker    ", font='FreeMono 50 bold',bg="#111")
-        self.title.pack(pady=10)
+        self.bg_image = ImageTk.PhotoImage(resized_image)
 
+        # Create a label to hold the background image
+        self.bg_label = tk.Label(self, image=self.bg_image)
+        self.bg_label.place(relx=0, y=0, relwidth=1, relheight=1)  # Make it full screen
+        
         # create the leaderboard frame
         self.create_leaderboard_frame()
         self.create_player_input_section()
-
-        # Memuat gambar PNG 1
-        Image.MAX_IMAGE_PIXELS = None
-        self.image1 = Image.open(r"assets/decor/1.png")
-        self.new_size1 = (200, 200)  # Ukuran gambar yang diubah
-        self.resized_image1 = self.image1.resize(self.new_size1, Image.Resampling.LANCZOS)
-        self.photo1 = ImageTk.PhotoImage(self.resized_image1)
-        self.image_label1 = tk.Label(self, image=self.photo1, bg="#111", highlightthickness=0)
-        self.image_label1.place(relx=0.85, rely=0.99, anchor=tk.SW, width=200, height=200)
-
-        # Memuat gambar PNG 2
-        self.image2 = Image.open(r"assets/decor/2.png")
-        self.new_size2 = (200, 200)  # Ukuran gambar yang diubah
-        self.resized_image2 = self.image2.resize(self.new_size2, Image.Resampling.LANCZOS)
-        self.photo2 = ImageTk.PhotoImage(self.resized_image2)
-        self.image_label2 = tk.Label(self, image=self.photo2, bg="#111", highlightthickness=0)
-        self.image_label2.place(relx=0.70, rely=0.99, anchor=tk.SW, width=200, height=200)
-
-        # Memuat gambar PNG 3
-        self.image3 = Image.open(r"assets/decor/3.png")
-        self.new_size3 = (200, 200)  # Ukuran gambar yang diubah
-        self.resized_image3 = self.image3.resize(self.new_size3, Image.Resampling.LANCZOS)
-        self.photo3 = ImageTk.PhotoImage(self.resized_image3)
-        self.image_label3 = tk.Label(self, image=self.photo3, bg="#111", highlightthickness=0)
-        self.image_label3.place(relx=0.20, rely=0.99, anchor=tk.SW, width=200, height=200)
-
-        # Memuat gambar PNG 4
-        self.image4 = Image.open(r"assets/decor/4.png")
-        self.new_size4 = (200, 200)  # Ukuran gambar yang diubah
-        self.resized_image4 = self.image4.resize(self.new_size4, Image.Resampling.LANCZOS)
-        self.photo4 = ImageTk.PhotoImage(self.resized_image4)
-        self.image_label4 = tk.Label(self, image=self.photo4, bg="#111", highlightthickness=0)
-        self.image_label4.place(relx=0.01, rely=0.99, anchor=tk.SW, width=200, height=200)
-
-        # Memuat gambar PNG 5
-        self.image5 = Image.open(r"assets/decor/Logo_Universitas_Brawijaya.png")
-        self.new_size5 = (100,100)  # Ukuran gambar yang diubah
-        self.resized_image5 = self.image5.resize(self.new_size5, Image.Resampling.LANCZOS)
-        self.photo5 = ImageTk.PhotoImage(self.resized_image5)
-        self.image_label5 = tk.Label(self, image=self.photo5, bg="#111", highlightthickness=0)
-        self.image_label5.place(relx=0.01, rely=0.13, anchor=tk.SW, width=100, height=100)
-
-        # Memuat gambar PNG 4
-        self.image6 = Image.open(r"assets/decor/Filkom_UB.png")
-        self.new_size6 = (200, 100)  # Ukuran gambar yang diubah
-        self.resized_image6 = self.image6.resize(self.new_size6, Image.Resampling.LANCZOS)
-        self.photo6 = ImageTk.PhotoImage(self.resized_image6)
-        self.image_label6 = tk.Label(self, image=self.photo6, bg="#111", highlightthickness=0)
-        self.image_label6.place(relx=0.08, rely=0.13, anchor=tk.SW, width=200, height=100)
-
-        # Memuat gambar PNG 4
-        self.image7 = Image.open(r"assets/decor/Logo_Robotiik_HD_Logo.png")
-        self.new_size7 = (100, 100)  # Ukuran gambar yang diubah
-        self.resized_image7 = self.image7.resize(self.new_size7, Image.Resampling.LANCZOS)
-        self.photo7 = ImageTk.PhotoImage(self.resized_image7)
-        self.image_label7 = tk.Label(self, image=self.photo7, bg="#111", highlightthickness=0)
-        self.image_label7.place(relx=0.21, rely=0.13, anchor=tk.SW, width=100, height=100)
 
     def create_player_input_section(self):
         # Input fields (Player Name and Cluster)
 
         # Name input
-        self.name_frame = tk.Frame(self,bg="#111")
+        self.name_frame = tk.Frame(self, bg="gold2")
         self.name_frame.pack(pady=5)
-        self.name_frame.place (relx=0.516, rely=0.7, anchor=tk.CENTER, width=240, height=30)
-        self.name_label = tk.Label(self.name_frame, font='Arial 17 bold',fg="snow",text="NAMA :",bg="#111")
+        self.name_frame.place(relx=0.512, rely=0.79, anchor=tk.CENTER, width=240, height=30)
+        self.name_label = tk.Label(self.name_frame, font='Arial 17 bold', fg="#384987", text="NAMA :", bg="gold2")
         self.name_label.pack(side=tk.LEFT)
         self.name_entry = tk.Entry(self.name_frame, width=30)
         self.name_entry.pack(side=tk.LEFT, padx=10)
 
-        #cluster dropdown
-        self.cluster_frame = tk.Frame(self,bg="#111")
+        # Cluster dropdown
+        self.cluster_frame = tk.Frame(self, bg="gold2")
         self.cluster_frame.pack(pady=5)
-        self.cluster_frame.place(relx=0.5, rely=0.76, anchor=tk.CENTER, width=300, height=40)
-        self.cluster_dropdown = DropdownMenu(self.cluster_frame, ["Cluster 1", "Cluster 2", "Cluster 3", "Cluster 4", "Cluster 5", "Cluster 6", "Cluster 7", "Cluster 8", "Cluster 9", "Cluster 10", "Cluster 11", "Cluster 12", "Cluster 13", "Cluster 14", "Cluster 15", "Cluster 16", "Cluster 17", "Cluster 18", "Cluster 19", "Cluster 20", "Cluster 21", "Cluster 22"])
+        self.cluster_frame.place(relx=0.5, rely=0.84, anchor=tk.CENTER, width=300, height=40)
+        self.cluster_dropdown = DropdownMenu(self.cluster_frame, ["Cluster 1", "Cluster 2", "Cluster 3", "Cluster 4", 
+                                                                  "Cluster 5", "Cluster 6", "Cluster 7", "Cluster 8", 
+                                                                  "Cluster 9", "Cluster 10", "Cluster 11", "Cluster 12", 
+                                                                  "Cluster 13", "Cluster 14", "Cluster 15", "Cluster 16", 
+                                                                  "Cluster 17", "Cluster 18", "Cluster 19", "Cluster 20", 
+                                                                  "Cluster 21", "Cluster 22"])
 
-        # Start button
-        self.start_button = tk.Button(self, text="START", font='FreeMono 17',command=self.start_game,bg="#EC008C")
+        # Load start button image
+        start_image = Image.open(r"assets/decor/start.png")
+        start_image_resized = start_image.resize((150, 40), Image.LANCZOS)  # Resize if needed
+        self.start_image_tk = ImageTk.PhotoImage(start_image_resized)
+
+        # Start button with image
+        self.start_button = tk.Button(self, image=self.start_image_tk, command=self.start_game, bg="#EC008C", bd=0)
         self.start_button.pack(pady=30)
-        self.start_button.place(relx=0.5, rely=0.85, anchor=tk.CENTER, width=100, height=30)
+        self.start_button.place(relx=0.5, rely=0.92, anchor=tk.CENTER, width=150, height=40)
+
 
     def create_leaderboard_frame(self):
-         #Create the leaderboard frame with background color
-        leaderboard_frame = tk.Frame(self, bg="gold2", bd=0)
-        leaderboard_frame.place(relx=0.5, rely=0.4, anchor=tk.CENTER, width=600, height=450)
-
-        # Label for the leaderboard
-        leaderboard_label = tk.Label(leaderboard_frame, text="LEADERBOARD", bg="gold2", fg="black",font='Arial 23 bold')
-        leaderboard_label.pack(pady=10)
 
         # Box for leaderboard content
-        leaderboard_box = tk.Frame(leaderboard_frame, bg="gold2", bd=0)
-        leaderboard_box.place(relx=0.5, rely=0.55, anchor=tk.CENTER, width=580, height=380)
+        leaderboard_box = tk.Frame(self, bg="gold2", bd=0)
+        leaderboard_box.place(relx=0.5, rely=0.47, anchor=tk.CENTER, width=580, height=380)
 
         # Scrollable leaderboard display
         self.leaderboard_display = ScrollableLeaderboard(leaderboard_box)
